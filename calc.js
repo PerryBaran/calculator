37ed9cb5d628c115476997a9abb88fc7a4e1997c
Perry <petreli1@ymail.com>
2021-09-28 13:40:22 +0100

    added basic functionality to calculator, still to add del and clear functionality

--- calc.js
+++ calc.js
@@ -1,3 +1,12 @@
+const numberInput = document.querySelectorAll('.number');
+const operatorInput = document.querySelectorAll('.operators');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+const del = document.querySelector('.del');
+const displayTop = document.querySelector('.top');
+const displayBottom = document.querySelector('.bottom');
+
+
 function add(a, b) {
     return a + b;
 };
@@ -14,12 +23,18 @@ function divide(a, b){
     if (b === 0){
         return 'nah';
     } else {
-    return a / b;
+        return a / b;
     }
 };
 
 function power(a, b){
     return a ** b;
+};
+
+function clearDisplay(){
+    displayTop.innerHTML = '';
+    displayBottom.innerHTML = '';
+    operatorInput = undefined;
 }
 
 function operate(operator, a, b) {
@@ -36,3 +51,27 @@ function operate(operator, a, b) {
     }
 };
 
+function append(number){
+    displayBottom.innerHTML = displayBottom.innerHTML.toString() + number.toString();
+};
+
+numberInput.forEach(button => {
+    button.addEventListener('click', () => {
+        append(button.innerHTML);
+    });
+});
+
+operatorInput.forEach(button => {
+    button.addEventListener('click', () => {
+        displayTop.innerHTML = displayBottom.innerHTML + ' ' + button.innerHTML.toString();
+        displayBottom.innerHTML = '';
+    });
+});
+
+equals.addEventListener('click', () => {
+    const operator = displayTop.innerHTML.slice(-1)
+    const a = displayTop.innerHTML.slice(0, -2);
+    const b = displayBottom.innerHTML;
+    displayTop.innerHTML = operate(operator, a, b);
+    displayBottom.innerHTML = '';
+});
